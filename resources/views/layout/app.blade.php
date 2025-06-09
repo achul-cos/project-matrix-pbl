dddada7d80ae63d3a85565b7a025d15e01c37d14
ARA_BELLA_GINTING <arabella061006@gmail.com>
2025-06-10 01:03:01 +0700

    Ara 10/06/2025 final FAQ 01:02

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -8,20 +8,11 @@
     <title>@yield('title', 'Laravel app')</title>
     <!-- Stylesheets -->
     <link href="styles/flowbite.min.css" rel="stylesheet">
-    <script src="https://unpkg.com/flowbite@1.6.5/dist/flowbite.min.js"></script>
     @vite('resources/css/app.css')
-    <!-- Tailwind CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <!-- Flowbite CSS -->
-    <link href="https://unpkg.com/flowbite@1.6.5/dist/flowbite.min.css" rel="stylesheet" />
-
 </head>
-<!-- Flowbite JS -->
-<script src="https://unpkg.com/flowbite@1.6.5/dist/flowbite.min.js"></script>
-
 <body class="bg-white inter">
     @include('components.navbar')
-    <div class="pt-20 min-h-dvh pb-10">
+    <div class="pt-20 mi    n-h-dvh pb-10">
         @yield('content')
     </div>
     @include('components.footer')
@@ -29,5 +20,7 @@
     <script src="styles/flowbite.min.js"></script>
     <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
     @vite('resources/js/app.js')
+    <!-- Flowbite JS -->
+    <script src="https://unpkg.com/flowbite@1.6.5/dist/flowbite.min.js"></script>
 </body>
 </html>     
